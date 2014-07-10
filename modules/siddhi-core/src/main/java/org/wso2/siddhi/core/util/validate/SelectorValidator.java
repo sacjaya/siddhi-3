7004fa90c9f97c7f691d6729266af07ea2f20fed
tishan89 <dtishanpubudu@gmail.com>
2014-07-10 17:29:20 +0530

    adding complex attribute validation and creating inferred streams

--- modules/siddhi-core/src/main/java/org/wso2/siddhi/core/util/validate/SelectorValidator.java
+++ modules/siddhi-core/src/main/java/org/wso2/siddhi/core/util/validate/SelectorValidator.java
@@ -12,7 +12,10 @@
  */
 package org.wso2.siddhi.core.util.validate;
 
+import org.wso2.siddhi.core.exception.OperationNotSupportedException;
 import org.wso2.siddhi.core.exception.ValidatorException;
+import org.wso2.siddhi.core.util.Constants;
+import org.wso2.siddhi.query.api.definition.Attribute;
 import org.wso2.siddhi.query.api.definition.StreamDefinition;
 import org.wso2.siddhi.query.api.expression.Expression;
 import org.wso2.siddhi.query.api.expression.Variable;
@@ -21,13 +24,14 @@ import org.wso2.siddhi.query.api.query.selection.attribute.ComplexAttribute;
 import org.wso2.siddhi.query.api.query.selection.attribute.OutputAttribute;
 import org.wso2.siddhi.query.api.query.selection.attribute.SimpleAttribute;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
 public class SelectorValidator {
 
     /**
-     * Method to validate selector for given set of stream definitions
+     * Method to validate selector for given set of stream definitions     *
      *
      * @param selector:       Selector to validate
      * @param definitionList: Definitions to validate against
@@ -35,16 +39,27 @@ public class SelectorValidator {
      * @throws ValidatorException Thrown if not validated
      */
     public static void validate(Selector selector, List<StreamDefinition> definitionList, Map<String, String> renameMap) throws ValidatorException {
+        StreamDefinition temp = new StreamDefinition(Constants.SELECTOR_STREAM);        //inferred stream
         for (OutputAttribute attribute : selector.getSelectionList()) {
             if (attribute instanceof SimpleAttribute) {
                 if (((SimpleAttribute) attribute).getExpression() instanceof Variable) {
                     ValidatorUtil.handleVariable((Variable) ((SimpleAttribute) attribute).getExpression(), definitionList, renameMap);
+                    StreamDefinition source = getDefinitionFor(((Variable) ((SimpleAttribute) attribute).getExpression()).getStreamId(), definitionList);
+                    String name = ((Variable) ((SimpleAttribute) attribute).getExpression()).getAttributeName();
+                    temp.attribute(attribute.getRename(), ValidatorUtil.getAttributeType(name, source));
                 }//TODO: handle else
             } else if (attribute instanceof ComplexAttribute) {          //TODO:check if we need to validate attribute names
                 for (Expression expression : ((ComplexAttribute) attribute).getExpressions()) {
-                    if (expression instanceof Variable) {
-                        ValidatorUtil.handleVariable((Variable) ((SimpleAttribute) attribute).getExpression(), definitionList, renameMap);
+                    ValidatorUtil.validateCompareExpression(expression, definitionList, renameMap);
+                    //Assuming aggregator complex attributes will not get a expression list
+                    Attribute.Type type = ValidatorUtil.getAttributeType(((Variable) expression).getAttributeName(), getDefinitionFor(((Variable) expression).getStreamId(), definitionList));
+                    Attribute.Type result;
+                    try {
+                        result = ValidatorUtil.getAggregatorReturnType(((ComplexAttribute) attribute).getAttributeName(), type);
+                    } catch (OperationNotSupportedException e) {
+                        throw new ValidatorException(e.getMessage());
                     }
+                    temp.attribute(attribute.getRename(), result);
                 }
             }
         }
@@ -53,8 +68,18 @@ public class SelectorValidator {
                 ValidatorUtil.handleVariable(var, definitionList, renameMap);
             }
         }
-        if (selector.getHavingCondition() != null) {                    //Handle having condition
-            ValidatorUtil.validateCondition(selector.getHavingCondition(), definitionList, renameMap);
+        if (selector.getHavingCondition() != null) {                    //Handle having condition. send only the inferred stream
+            ValidatorUtil.validateCondition(selector.getHavingCondition(), Arrays.asList(temp), renameMap);
         }
+        definitionList.add(temp);
+    }
+
+    private static StreamDefinition getDefinitionFor(String streamId, List<StreamDefinition> definitionList) {
+        for (StreamDefinition definition : definitionList) {
+            if (definition.getStreamId().equals(streamId)) {
+                return definition;
+            }
+        }
+        return null;
     }
 }
