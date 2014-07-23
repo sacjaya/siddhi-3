21f748f41c5c51499378e17a647c03806f95d7e4
tishan89 <dtishanpubudu@gmail.com>
2014-07-23 19:00:33 +0530

    commiting event interface

--- modules/siddhi-core/src/main/java/org/wso2/siddhi/core/event/Event.java
+++ modules/siddhi-core/src/main/java/org/wso2/siddhi/core/event/Event.java
@@ -1,74 +1,48 @@
 /*
-*  Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005 - 2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.wso2.siddhi.core.event;
 
-import java.util.Arrays;
+/**
+ * This is the event interface exposed to the user.
+ */
+public interface Event {
+    /**
+     * This should return the timestamp associated with the event.
+     *
+     * @return
+     */
+    long getTimeStamp();
 
-public class Event implements StreamEvent{
+    /**
+     * This should return the object[] which holds attribute values of the event
+     *
+     * @return
+     */
+    Object[] getData();
 
-     private long timeStamp = -1;
-    private Object[] data = new Object[1];
+    /**
+     * This should return object in the ith position of attribute array.
+     *
+     * @param i array index
+     * @return
+     */
+    Object getData(int i);
 
-    public Event( long timeStamp, Object[] data) {
-        this.timeStamp = timeStamp;
-        this.data = data;
-    }
-
-    public Event(){
-    }
-
-    public Event(int dataSize){
-        this.data = new Object[dataSize];
-    }
-
-
-    public long getTimeStamp() {
-        return timeStamp;
-    }
-
-    public Object[] getData() {
-        return data;
-    }
-
-    public Object getData(int i) {
-        return data[i];
-    }
-
-    @Override
-    public String toString() {
-        return "Event{" +
-                " timeStamp=" + timeStamp +
-                ", data=" + (data == null ? null : Arrays.asList(data)) +
-                '}';
-    }
-
-    @Override
-    public Event[] toArray() {
-        return new Event[]{this};
-    }
-
-    public void setData(Object[] data) {
-        this.data =data;
-    }
-
-    public void setTimeStamp(long timeStamp) {
-        this.timeStamp = timeStamp;
-    }
-
-
+    /**
+     * This should return whether the message is expired
+     *
+     * @return
+     */
+    Boolean isExpired();
 }
