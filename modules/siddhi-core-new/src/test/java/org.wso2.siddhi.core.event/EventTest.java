28da814335451c692e20b72e556ec73f86f26435
tishan89 <dtishanpubudu@gmail.com>
2014-08-14 12:21:42 +0530

    adding EventConverterTest

--- modules/siddhi-core-new/src/test/java/org.wso2.siddhi.core.event/EventTest.java
+++ modules/siddhi-core-new/src/test/java/org.wso2.siddhi.core.event/EventTest.java
@@ -2,9 +2,15 @@ package org.wso2.siddhi.core.event;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.wso2.siddhi.core.event.inner.InnerStreamEvent;
+import org.wso2.siddhi.core.event.inner.InnerStreamEventConverter;
+import org.wso2.siddhi.core.event.inner.MetaInnerStreamEvent;
 import org.wso2.siddhi.core.event.stream.StreamEventFactory;
 import org.wso2.siddhi.core.event.inner.InnerStreamEventPool;
 import org.wso2.siddhi.core.event.stream.StreamEvent;
+import org.wso2.siddhi.core.query.selector.attribute.ComplexAttribute;
+import org.wso2.siddhi.query.api.definition.Attribute;
+import org.wso2.siddhi.query.api.definition.StreamDefinition;
 
 /**
  * Created by suho on 8/13/14.
@@ -37,4 +43,35 @@ public class EventTest {
 //        Assert.assertEquals(3, innerStreamEventPool.getBufferedEventsSize());
 //
 //    }
+
+
+    @Test
+    public void testEventConverter() {
+        Attribute price = new Attribute("price", Attribute.Type.DOUBLE);
+        Attribute volume = new Attribute("volume", Attribute.Type.INT);
+        Attribute symbol = new Attribute("symbol", Attribute.Type.STRING);
+        ComplexAttribute avgPrice = new ComplexAttribute();
+
+        MetaInnerStreamEvent metaInnerStreamEvent = new MetaInnerStreamEvent();
+        metaInnerStreamEvent.addData(volume);
+        metaInnerStreamEvent.intializeAfterWindowData();
+        metaInnerStreamEvent.addData(price);
+        metaInnerStreamEvent.intializeOutData();
+        metaInnerStreamEvent.addData(symbol);
+        metaInnerStreamEvent.addData(avgPrice);
+
+        StreamDefinition streamDefinition = StreamDefinition.id("cseEventStream").attribute("symbol", Attribute.Type.STRING).attribute("price", Attribute.Type.DOUBLE).attribute("volume", Attribute.Type.INT);
+        StreamEvent event = new StreamEvent(System.currentTimeMillis(), new Object[]{"WSO2", 200, 50});
+
+        InnerStreamEventConverter converter = new InnerStreamEventConverter(metaInnerStreamEvent, streamDefinition);
+        InnerStreamEvent innerStreamEvent = converter.convertToInnerStreamEvent(event);
+
+        Assert.assertEquals(1, innerStreamEvent.getBeforeWindowData().length);      //volume
+        Assert.assertEquals(1, innerStreamEvent.getOnAfterWindowData().length);     //price
+        Assert.assertEquals(2, innerStreamEvent.getOutputData().length);            //symbol and avgPrice
+
+        Assert.assertEquals(50, innerStreamEvent.getBeforeWindowData()[0]);
+        Assert.assertEquals(200, innerStreamEvent.getOnAfterWindowData()[0]);
+        Assert.assertEquals("WSO2", innerStreamEvent.getOutputData()[0]);
+    }
 }
