aa4f881f01e9ad4009d1f1aa1059ec42f996e940
suhothayan <suhothayan@gmail.com>
2014-08-15 13:10:18 +0530

    improving processors

--- modules/siddhi-core-new/src/main/java/org/wso2/siddhi/core/query/processor/Processor.java
+++ modules/siddhi-core-new/src/main/java/org/wso2/siddhi/core/query/processor/Processor.java
@@ -1,12 +1,13 @@
 /*
- * Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2005 - 2014, WSO2 Inc. (http://www.wso2.org)
+ * All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -24,12 +25,15 @@ public interface Processor {
 
     public void process(StreamEvent event);
 
-    /**
-     * set next processor of the processor chain
-     * @param processor
-     */
+    public Processor getNext();
+
     public void setNext(Processor processor);
 
-    public Processor getNext();
+    /**
+     * Set as the last element of the processor chain
+     *
+     * @param processor the last processor in the chain
+     */
+    public void setToLast(Processor processor);
 
 }
--- modules/siddhi-core-new/src/main/java/org/wso2/siddhi/core/query/processor/filter/FilterProcessor.java
+++ modules/siddhi-core-new/src/main/java/org/wso2/siddhi/core/query/processor/filter/FilterProcessor.java
@@ -1,12 +1,13 @@
 /*
- * Copyright (c) 2005-2015, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * Copyright (c) 2005 - 2014, WSO2 Inc. (http://www.wso2.org)
+ * All Rights Reserved.
  *
  * WSO2 Inc. licenses this file to you under the Apache License,
  * Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
@@ -18,13 +19,15 @@
 package org.wso2.siddhi.core.query.processor.filter;
 
 import org.wso2.siddhi.core.event.stream.StreamEvent;
+import org.wso2.siddhi.core.executor.ExpressionExecutor;
 import org.wso2.siddhi.core.executor.condition.ConditionExpressionExecutor;
 import org.wso2.siddhi.core.query.processor.Processor;
 
 
 public class FilterProcessor implements Processor {
-    private ConditionExpressionExecutor conditionExecutor;
+
     protected Processor next;
+    private ExpressionExecutor conditionExecutor;
 
     public FilterProcessor(ConditionExpressionExecutor conditionExecutor) {
         this.conditionExecutor = conditionExecutor;
@@ -32,17 +35,8 @@ public class FilterProcessor implements Processor {
 
     @Override
     public void process(StreamEvent event) {
-        if (conditionExecutor.execute(event)) {
-                this.next.process(event);
-        }
-    }
-
-    @Override
-    public void setNext(Processor processor) {
-        if (next == null) {
-            this.next = processor;
-        } else {
-            (this.next).setNext(processor);
+        if ((Boolean) conditionExecutor.execute(event)) {
+            this.next.process(event);
         }
     }
 
@@ -50,4 +44,19 @@ public class FilterProcessor implements Processor {
     public Processor getNext() {
         return next;
     }
+
+    @Override
+    public void setNext(Processor processor) {
+        next = processor;
+    }
+
+    @Override
+    public void setToLast(Processor processor) {
+        if (next == null) {
+            this.next = processor;
+        } else {
+            (this.next).setToLast(processor);
+        }
+    }
+
 }
