ba24b9bea82bd0a752139e82b65634e8e92b8f1d
tishan89 <dtishanpubudu@gmail.com>
2014-09-01 15:06:24 +0530

    handing over partial ExecutionPlanParser

---
+++ modules/siddhi-core-new/src/main/java/org/wso2/siddhi/core/util/parser/ExecutionPlanParser.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2005 - 2014, WSO2 Inc. (http://www.wso2.org)
+ * All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.siddhi.core.util.parser;
+
+import org.wso2.siddhi.core.ExecutionPlanRuntime;
+import org.wso2.siddhi.core.config.SiddhiContext;
+import org.wso2.siddhi.query.api.ExecutionPlan;
+import org.wso2.siddhi.query.api.definition.StreamDefinition;
+import org.wso2.siddhi.query.api.execution.ExecutionElement;
+
+import java.util.Map;
+
+public class ExecutionPlanParser {
+    public static ExecutionPlanRuntime parse(ExecutionPlan executionPlan) {
+        SiddhiContext siddhiContext = new SiddhiContext();
+        ExecutionPlanRuntime executionPlanRuntime = new ExecutionPlanRuntime(siddhiContext);
+        defineStreamDefinitions(executionPlanRuntime, executionPlan.getStreamDefinitionMap());
+        for (ExecutionElement element : executionPlan.getExecutionElementList()) {
+            //create query runtimes and do necessary subscriptions
+        }
+        return null;
+    }
+
+    private static void defineStreamDefinitions(ExecutionPlanRuntime executionPlanRuntime, Map<String, StreamDefinition> streamDefinitionMap) {
+        for (StreamDefinition definition : streamDefinitionMap.values()) {
+            executionPlanRuntime.defineStream(definition);
+        }
+    }
+}
